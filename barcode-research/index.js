36e6d3777ebe80d0e764fed559aec824a3774bc7
syafisham <syafisham.salleh@sapuraenergy.com>
2020-09-22 15:57:24 +0800

    using vendorId and productId to connect

--- barcode-research/index.js
+++ barcode-research/index.js
@@ -5,28 +5,29 @@ var devices = HID.devices();
 console.log("Connected devices: ", devices)
 console.log("Connected devices path: ", devices[0].path)
 
-// const options = {
-//     vendorID:  1504,
-//     productID: 4608
-// }
+const options = {
+    vendorID:  0x1504,
+    productID: 0x4608
+}
 
 // const options = {
 // 	path: '/dev/hidraw0'
 // }
 
-var deviceInfo = devices.find( function(d) {
-    var isTeensy = d.vendorId===0x1504 && d.productId===0x4608;
-    return isTeensy;
-});
-if( deviceInfo ) {
+// var deviceInfo = devices.find( function(d) {
+//     var isTeensy = d.vendorId===0x1504 && d.productId===0x4608;
+//     return isTeensy;
+// });
 
-  console.log("Start the scanner>>>>");
-  var device = new HID.HID( deviceInfo.path );
+// if( deviceInfo ) {
 
-  device.on("data", function(data) {
-    console.log("Data from barcode:" , data);
-  });
-}
+//   console.log("Start the scanner>>>>");
+//   var device = new HID.HID( deviceInfo.path );
+
+//   device.on("data", function(data) {
+//     console.log("Data from barcode:" , data);
+//   });
+// }
 
 //   try{
     
@@ -39,18 +40,18 @@ if( deviceInfo ) {
 //   // ... use device
 // }
 
-// const scanner = new UsbScanner(options)
+const scanner = new UsbScanner(options)
 
-// scanner.on('data', (data) => {
-//     /// your code
-//     console.log("Data from barcode:" , data);
-// });
+scanner.on('data', (data) => {
+    /// your code
+    console.log("Data from barcode:" , data);
+});
 
-// try{
-//     console.log("Start the scanner>>>>");
-//     scanner.startScanning()
-// }
-// catch (e)
-// {
-//     console.log("Error when start the scanner: " , e);
-// }
+try{
+    console.log("Start the scanner>>>>");
+    scanner.startScanning()
+}
+catch (e)
+{
+    console.log("Error when start the scanner: " , e);
+}

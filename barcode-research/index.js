c7a315fdf5f861feb72c70e1131f8d86a00fe7fb
syafisham <syafisham.salleh@sapuraenergy.com>
2020-09-22 15:41:14 +0800

    update scanner

--- barcode-research/index.js
+++ barcode-research/index.js
@@ -1,5 +1,20 @@
+const UsbScanner = require('@isirthijs/barcode-scanner');
 var HID = require('node-hid');
 var devices = HID.devices();
 
 console.log("Connected devices: ", devices)
 console.log("Connected devices path: ", devices[0].path)
+
+const options = {
+    vendorID:  1504,
+    productID: 4608
+}
+
+const scanner = new UsbScanner(options)
+
+scanner.on('data', (data) => {
+    /// your code
+    console.log("Data from barcode:" , data);
+});
+
+scanner.startScanning()

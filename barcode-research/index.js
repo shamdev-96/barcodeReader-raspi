d9c378b7a94d53d0a9680d830c5f8173aa9011ed
syafisham <syafisham.salleh@sapuraenergy.com>
2020-09-22 16:13:53 +0800

    change again

--- barcode-research/index.js
+++ barcode-research/index.js
@@ -5,55 +5,45 @@ var devices = HID.devices();
 console.log("Connected devices: ", devices)
 console.log("Connected devices path: ", devices[0].path)
 
-const options = {
-    vendorID:  '05e0',
-    productID: '1200'
-}
+// const options = {
+//     vendorID:  '05e0',
+//     productID: '1200'
+// }
 
 // const options = {
 // 	path: '/dev/hidraw0'
 // }
 
-// var deviceInfo = devices.find( function(d) {
-//     var isTeensy = d.vendorId===0x1504 && d.productId===0x4608;
-//     return isTeensy;
+var deviceInfo = devices.find( function(d) {
+    var isTeensy = d.vendorId==='05e0' && d.productId==='1200';
+    return isTeensy;
+});
+
+if( deviceInfo ) {
+
+  console.log("Start the scanner>>>>");
+  var device = new HID.HID( deviceInfo.path );
+
+  device.on("data", function(data) {
+    console.log("Data from barcode:" , data);
+  });
+}
+
+
+// const scanner = new UsbScanner(options)
+// var deviceScanner = UsbScanner.showDevices()
+// console.log("List of devices:" , deviceScanner);
+
+// scanner.on('data', (data) => {
+//     /// your code
+//     console.log("Data from barcode:" , data);
 // });
 
-// if( deviceInfo ) {
-
-//   console.log("Start the scanner>>>>");
-//   var device = new HID.HID( deviceInfo.path );
-
-//   device.on("data", function(data) {
-//     console.log("Data from barcode:" , data);
-//   });
-// }
-
-//   try{
-    
+// try{
+//     console.log("Start the scanner>>>>");
 //     scanner.startScanning()
 // }
 // catch (e)
 // {
 //     console.log("Error when start the scanner: " , e);
 // }
-//   // ... use device
-// }
-
-const scanner = new UsbScanner(options)
-var deviceScanner = UsbScanner.showDevices()
-console.log("List of devices:" , deviceScanner);
-
-scanner.on('data', (data) => {
-    /// your code
-    console.log("Data from barcode:" , data);
-});
-
-try{
-    console.log("Start the scanner>>>>");
-    scanner.startScanning()
-}
-catch (e)
-{
-    console.log("Error when start the scanner: " , e);
-}

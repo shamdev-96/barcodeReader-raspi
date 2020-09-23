0cd0f711066c30aa0cb899fb3522ffd73dcff72a
syafisham <syafisham.salleh@sapuraenergy.com>
2020-09-23 12:01:55 +0800

    refractor code

---
+++ barcode-research/barcode-scanner.js
@@ -0,0 +1,115 @@
+const HID = require('node-hid').HID;
+const devices = require('node-hid').devices();
+const EventEmitter = require('events');
+const hidMap = require('./hidmap');
+
+class BarcodeScanner extends EventEmitter {
+
+	constructor(options) {
+		options = options || {};
+
+		let { 
+			vendorID = undefined,
+			productID = undefined,
+			path = undefined, 
+			vCardString = true,
+			vCardSeperator = '|'
+		} = options;
+
+		super();
+
+		this.vendorID = vendorID;
+		this.productID = productID;
+		this.path = path;
+		this._vCardString = vCardString;
+		this._vCardSeperator = vCardSeperator;
+
+		this._hidMap = hidMap.standard;
+		this._hidMapShift = hidMap.shift;
+
+		// Bind 'this' to the methods
+		this.startScanning = this.startScanning.bind(this);
+	}
+
+	static showDevices() {
+		return devices;
+	}
+
+	startScanning() {
+		
+		console.log("Start the scanner>>>>");
+
+		try {
+			if(this.path) 
+			{
+				this.hid = new HID(path);
+				console.log("---Scanner is Succesfully Registered---");
+			}
+			else if (this.vendorID && this.productID)
+			{
+				this.hid = new HID(this.vendorID, this.productID);
+				console.log("---Scanner is Succesfully Registered---");
+			} 
+			else 
+			{
+				throw 'Device cannot be found, please supply a path or VID & PID';
+			}
+		} catch(error) {
+			this.emit('error', error);
+		}
+		
+		let scanResult = [];
+		let vCard = [];
+
+		this.hid.on('data', (data) => {
+
+			const modifierValue = data[0];
+			const characterValue = data[2];
+
+			if (characterValue !== 0) {
+				if (modifierValue === 2 || modifierValue === 20) {
+					scanResult.push(this._hidMapShift[characterValue]);
+				} else if (characterValue !== 40) {
+					scanResult.push(this._hidMap[characterValue]);
+				} else if (characterValue === 40) {
+					let barcode = scanResult.join('');
+					scanResult = [];
+
+					barcode = removeUTF8(barcode);
+					
+					if (this._vCardString) {
+						if (barcode === 'BEGIN:VCARD') {
+							vCard.push(barcode);
+						} else if (barcode === 'END:VCARD') {
+							vCard.push(barcode);
+							vCard = vCard.join(this._vCardSeperator);
+							this.emit('data', vCard);
+							vCard = [];
+						} else if (vCard.length > 0 ) {
+							vCard.push(barcode);
+						} else this.emit('data', barcode);
+					} else {
+						this.emit('data', barcode);
+					}
+				}
+			}
+		});
+		
+	}
+
+	stopScanning() {
+		this.hid.removeAllListeners('data');
+		this.hid.close();
+	}
+
+}
+
+function removeUTF8(barcode) {
+	let utf8 = barcode.slice(0, 7);
+	if (utf8 === '\\000026') {
+		barcode = barcode.slice(7);
+		return barcode;
+	} else return barcode;
+}
+
+module.exports = BarcodeScanner;
--- barcode-research/index.js
+++ barcode-research/index.js
@@ -1,11 +1,10 @@
-const UsbScanner = require("@isirthijs/barcode-scanner");
+//const UsbScanner = require("@isirthijs/barcode-scanner");
 var HID = require("node-hid");
 const hidMap = require("./hidmap");
-var atob = require("atob");
+const EventEmitter = require('events');
+const BarcodeScanner = require('./barcode-scanner')
 var devices = HID.devices();
 
-let _hidMap = hidMap.standard;
-let _hidMapShift = hidMap.shift;
 
 var barcodeDevice = devices.find(item => item.product.includes("Scanner"))
 console.log("Barcode device is: " , barcodeDevice );
@@ -13,65 +12,17 @@ var device = new HID.HID(barcodeDevice.vendorId, barcodeDevice.productId);
 
 
 
-
-// let byteData = []
-// device.on("data",  (data) => {
-//   console.log("Data from barcode:", byteData);
-// });
-
-
-let scanResult = [];
-let vCard = [];
-
-console.log("Start the scanner>>>>");
-
-device.on('data', function (data) {
-
-  const modifierValue = data[0];
-  const characterValue = data[2];
-
-  if (characterValue !== 0) {
-    if (modifierValue === 2 || modifierValue === 20) {
-      scanResult.push(_hidMapShift[characterValue]);
-    } else if (characterValue !== 40) {
-      scanResult.push(_hidMap[characterValue]);
-    } else if (characterValue === 40) {
-      let barcode = scanResult.join('');
-      scanResult = [];
-      barcode = removeUTF8(barcode);
-      console.log("Data from barcode:", barcode);
-
-      if (barcode === 'BEGIN:VCARD') {
-        vCard.push(barcode);
-        console.log("Data from barcode:", vCard);
-      } else if (barcode === 'END:VCARD') {
-        vCard.push(barcode);
-        vCard = vCard.join('|');
-        console.log("Data from barcode:", vCard);
-        vCard = [];
-      } else if (vCard.length > 0) {
-        vCard.push(barcode);
-      } else {
-        console.log("Data from barcode:", barcode);
-      }
-    }
-  }
-  
-});
-
-
-function removeUTF8(barcode) {
-  let utf8 = barcode.slice(0, 7);
-  if (utf8 === '\\000026') {
-    barcode = barcode.slice(7);
-    return barcode;
-  } else return barcode;
+const options = {
+    vendorID:  barcodeDevice.vendorId,
+    productID: barcodeDevice.productId
 }
 
-// const options = {
-//     vendorID:  barcodeDevice.vendorId,
-//     productID: barcodeDevice.productId
-// }
+const scanner = new BarcodeScanner(options)
+
+scanner.on('data', (data) => 
+console.log("Data from barcode:" , data)); //eslint-disable-line
+
+scanner.startScanning();
 
 // const scanner = new UsbScanner(options)
 

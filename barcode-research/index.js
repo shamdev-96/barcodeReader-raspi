44b389c3ced46d4f8803cf5b68286e9a4e675dd6
syafisham <syafisham.salleh@sapuraenergy.com>
2020-09-23 11:47:55 +0800

    attempt 3

--- barcode-research/index.js
+++ barcode-research/index.js
@@ -7,69 +7,62 @@ var devices = HID.devices();
 let _hidMap = hidMap.standard;
 let _hidMapShift = hidMap.shift;
 
-console.log("Connected devices: ", devices);
-console.log("Connected devices path: ", devices[0].path);
-
-
-
-console.log("Start the scanner>>>>");
-
 var barcodeDevice = devices.find(item => item.product.includes("Scanner"))
 console.log("Barcode device is: " , barcodeDevice );
 var device = new HID.HID(barcodeDevice.vendorId, barcodeDevice.productId);
 
-// let scanResult = [];
-// let vCard = [];
 
-let byteData = []
-device.on("data",  (data) => {
-  while(data)
-  {
-    byteData.push(data)
-  }
-  console.log("Data from barcode:", byteData);
-});
-// device.on("data", function (data) {
 
-// //   const modifierValue = data[0];
-// //   const characterValue = data[2];
-// //   if (characterValue !== 0) {
-// //     if (modifierValue === 2 || modifierValue === 20) {
-// //       scanResult.push(_hidMapShift[characterValue]);
-// //     } else if (characterValue !== 40) {
-// //       scanResult.push(_hidMap[characterValue]);
-// //     } else if (characterValue === 40) {
-// //       let barcode = scanResult.join("");
-// //       scanResult = [];
-// //       barcode = removeUTF8(barcode);
-// //       console.log("Data from barcode:", barcode);
 
-// //       if (barcode === "BEGIN:VCARD") {
-// //         vCard.push(barcode);
-// //         console.log("Data from barcode:", vCard);
-// //       } else if (barcode === "END:VCARD") {
-// //         vCard.push(barcode);
-// //         vCard = vCard.join("|");
-// //         console.log("Data from barcode:", vCard);
-// //         vCard = [];
-// //       } else if (vCard.length > 0) {
-// //         vCard.push(barcode);
-// //         console.log("Data from barcode:", vCard);
-// //       } else {
-// //         console.log("Data from barcode:", vCard);
-// //       }
-// //     }
-
-//     console.log("-----------------------------------------------------------------");
-//     console.log("Data from barcode:" , data) 
-//     console.log("-----------------------------------------------------------------");
-  
+// let byteData = []
+// device.on("data",  (data) => {
+//   console.log("Data from barcode:", byteData);
 // });
+
+
+let scanResult = [];
+let vCard = [];
+
+console.log("Start the scanner>>>>");
+
+device.on('data', function (data) {
+
+  const modifierValue = data[0];
+  const characterValue = data[2];
+
+  if (characterValue !== 0) {
+    if (modifierValue === 2 || modifierValue === 20) {
+      scanResult.push(_hidMapShift[characterValue]);
+    } else if (characterValue !== 40) {
+      scanResult.push(_hidMap[characterValue]);
+    } else if (characterValue === 40) {
+      let barcode = scanResult.join('');
+      scanResult = [];
+      barcode = removeUTF8(barcode);
+      console.log("Data from barcode:", barcode);
+
+      if (barcode === 'BEGIN:VCARD') {
+        vCard.push(barcode);
+        console.log("Data from barcode:", vCard);
+      } else if (barcode === 'END:VCARD') {
+        vCard.push(barcode);
+        vCard = vCard.join('|');
+        console.log("Data from barcode:", vCard);
+        vCard = [];
+      } else if (vCard.length > 0) {
+        vCard.push(barcode);
+      } else {
+        console.log("Data from barcode:", barcode);
+      }
+    }
+  }
+  
+});
 
 
 function removeUTF8(barcode) {
   let utf8 = barcode.slice(0, 7);
-  if (utf8 === "\\000026") {
+  if (utf8 === '\\000026') {
     barcode = barcode.slice(7);
     return barcode;
   } else return barcode;
